7cc741c40367a8dd0c9dd80b87299a3344833a24
Curt <curt@stayfitcentral.com>
2025-04-12 16:41:47 -0700

    started user metadata plugin

---
+++ sfc-user-metadata.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: SFC Endura User Metadata
+Plugin URI: https://stayfitcentral.com
+Description: Captures and manages user metadata for personalized workouts in Endura.
+Version: 1.0
+Author: Jefe de los Jefes
+Author URI: https://stayfitcentral.com
+Text Domain: sfc-endura-user-metadata
+*/
